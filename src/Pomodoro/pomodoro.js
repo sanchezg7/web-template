cacd576c1e31a776ba0df2623b1142872fad15ca
ayogsynergy <sanchezg.eng@gmail.com>
2022-08-20 21:34:20 -0400

    restore real times for pomodoro

--- src/Pomodoro/pomodoro.js
+++ src/Pomodoro/pomodoro.js
@@ -1,4 +1,4 @@
 export const WORK = "WORK";
-export const WORK_SECONDS = 3; // 25 * 60;
+export const WORK_SECONDS = 25 * 60;
 export const REST = "REST";
-export const REST_SECONDS = 3; // 5 * 60;
+export const REST_SECONDS = 5 * 60;
